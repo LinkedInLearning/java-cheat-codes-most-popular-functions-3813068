b3b8a41e168883261876ebb41d61061cab54f741
semirahd <semirah.dolan@gmail.com>
2024-02-28 15:38:45 -0500

    Add files via upload

---
+++ scannerNextInt/src/com/example/scannernextint/Answer.java
@@ -0,0 +1,14 @@
+package com.example.scannernextint;
+
+public class Answer {
+
+    private int userInputInt;
+
+    public Answer(int userInputInt) {
+        this.userInputInt = userInputInt;
+    }
+
+    public int getUserInputInt() {
+        return userInputInt;
+    }
+}
